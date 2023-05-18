f45602969f422adbb8b2beae4189bd28719a799f
Roshinraj-T <roshinraj432@gmail.com>
2023-05-18 18:46:48 +0530

    added angular practies file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
